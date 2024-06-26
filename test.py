69b2caf05e824c3ffdfaabe14194caf378912814
miaordh <miaordh@foxmail.com>
2024-06-26 10:56:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -57,32 +57,39 @@ class TestFAHE2(unittest.TestCase):
         self.k, self.ek, self.dk = keygen2(128, self.m_max, 6)
         
     def test_keygen(self):
-        m_max = 10
-        m = secrets.randbelow(2**m_max - 1)
-        print("\nEncode: ", m)
-        k, ek, dk = keygen2(8, m_max, 10)
-        c = enc2(ek, m)
+        m = secrets.randbelow(2**self.m_max - 1)
+        print("\nEncoding: ", m)
+        c = enc2(self.ek, m)
         c_length = c.bit_length()
-        print("No. of bits of c:", c_length)
-        m_outcome = dec2(dk, c)
-        print("Decode: ", m_outcome)
+        print("Length of cyphertext (in bits):", c_length)
+        m_outcome = dec2(self.dk, c)
+        print("Decoding: ", m)
+        self.assertEqual(m, m_outcome, "Decoded message does not match the original")
 
     def test_add(self):
-        m_max = 10
-        m1 = secrets.randbelow(2**m_max - 1)
+        """Test encoding of two messages, their sum, and validate the addition operations. NOTE: alpha should be > than m_max to maintain additivity."""
+        m1 = secrets.randbelow(2**self.m_max - 1)
         c1 = enc2(self.ek, m1)
-        print("Encode m1: ", m1)
-        m2 = secrets.randbelow(2**m_max - 1)
+        m2 = secrets.randbelow(2**self.m_max - 1)
         c2 = enc2(self.ek, m2)
-        print("Encode m2: ", m2)
         m_sum = m1 + m2
-        print("Sum of m, m_sum:", m_sum)
+        print("\nEncoding Sum: ", m_sum)
         c_from_m_sum = enc2(self.ek, m_sum)
         c_from_adding = c1 + c2
         m_outcome_from_c_m_sum = dec2(self.dk, c_from_m_sum)
         m_outcome_from_c_adding = dec2(self.dk, c_from_adding)
-        print("Decode m_sum from c directly from m_sum: ", m_outcome_from_c_m_sum)
-        print("Decode m_sum from c from adding up c1 and c2: ", m_outcome_from_c_adding)
+        print("Decoding Sum: ", m_outcome_from_c_m_sum)
+        print("Direct Sum: ", m_outcome_from_c_adding)
+        self.assertEqual(
+            m_sum,
+            m_outcome_from_c_m_sum,
+            "Decoded sum does not match the actual sum from encoded messages",
+        )
+        self.assertEqual(
+            m_outcome_from_c_m_sum,
+            m_outcome_from_c_adding,
+            "Direct sum and encoded sum results differ",
+        )
 
 
 if __name__ == "__main__":
