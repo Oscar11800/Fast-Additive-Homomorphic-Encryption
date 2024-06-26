c2445588f6a911f3ef03fefef26fc68b94165f8c
miaordh <miaordh@foxmail.com>
2024-06-26 10:17:11 +0800

    Update fahe2.py
    Update methods; use secrets

--- fahe2.py
+++ fahe2.py
@@ -7,7 +7,7 @@ import random
 from typing import Tuple
 
 
-def keygen(l, m_max, alpha) -> tuple[float]:
+def keygen2(l, m_max, alpha) -> tuple[float]:
     """
     Generates a FAHE2 key.
 
@@ -23,10 +23,10 @@ def keygen(l, m_max, alpha) -> tuple[float]:
 
     rho = l + alpha + m_max
     eta = rho + alpha
-    gamma = rho / math.log2(rho) * ((eta - rho) ** 2)
-    p = helper.generate_large_prime(eta)
-    X = (Decimal(2) ** (Decimal(gamma))) / p
-    pos = random.randint(0, l)
+    gamma = rho / math.log2(rho) * ((eta - rho)**2)
+    p = helpfunctions.generate_large_prime(eta)
+    X = (Decimal(2) ** Decimal(gamma)) / p
+    pos = secrets.randbelow(l+1)
 
     k = (p, X, pos, m_max, l, alpha)
     ek = (p, X, pos, m_max, l, alpha)
@@ -34,7 +34,7 @@ def keygen(l, m_max, alpha) -> tuple[float]:
     return k, ek, dk
 
 
-def enc(ek, m):
+def enc2(ek, m):
     """
     Encrypts a messsage using FAHE2 scheme.
 
@@ -45,16 +45,16 @@ def enc(ek, m):
     Returns:
         c (float): ciphertext
     """
-    q = random.randrange(0, int(ek[1]))
-    noise1 = secrets.randbelow(2 ** ek[2] - 1)
-    noise2 = secrets.randbelow(2 ** (ek[4] - ek[2]) - 1)
-    M = (noise2 << (ek[2] + ek[3] + ek[5])) + (m << (ek[2] + ek[5])) + noise1
+    q = secrets.randbelow(int(ek[1])+1)
+    noise1 = secrets.randbelow(2**ek[2])
+    noise2 = secrets.randbelow(2**(ek[4]-ek[2]))
+    M = (noise2 << (ek[2]+ek[3]+ek[5])) + (m << (ek[2]+ek[5])) + noise1
     n = ek[0] * q
     c = n + M
     return c
 
 
-def dec(dk, c):
+def dec2(dk, c):
     """
     Decrypts a messsage using FAHE2 scheme.
 
@@ -66,6 +66,6 @@ def dec(dk, c):
         m (float): decrypted message (least significant bits)
     """
     m_full_string = bin((c % dk[0]) >> (dk[1] + dk[3]))
-    m = int(m_full_string[len(m_full_string) - m_max :], 2)
+    m = int(m_full_string[len(m_full_string)-m_max:],2)
     return m
 
